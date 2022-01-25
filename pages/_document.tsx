f67a4508325bf7d7a16228a8c6f54c10c896fcc4
Jan Klimo <jan.klimo@gmail.com>
2022-01-26 00:19:29 +0700

    Add config, format

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from "next/document"
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html className="h-full">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&family=Source+Sans+Pro:wght@300&family=VT323&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Poppins:wght@500&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body className="h-full bg-black-light font-body">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,65 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+  darkMode: "media",
   theme: {
-    extend: {},
+    fontFamily: {
+      body: [
+        "Montserrat",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+      ],
+      title: [
+        "Poppins",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+      ],
+    },
+    boxShadow: {
+      DEFAULT: "0px 3px 20px 15px rgba(0, 0, 0, 0.1);",
+    },
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: "#031316",
+          light: "#0f1f22",
+          lighter: "#172628",
+        },
+        orange: {
+          DEFAULT: "#e1552f",
+          light: "#ff9375",
+          lightest: "#ff807533",
+        },
+        gray: {
+          light: "#c0c4c5",
+          lighter: "#e0e1e2",
+          outline: "#2b383b",
+          "outline-light": "#869498",
+          dark: "#1f2d30",
+        },
+        green: {
+          DEFAULT: "#48c248",
+          light: "#5eed5d",
+        },
+      },
+      scale: {
+        "-1": "-1",
+      },
+      borderWidth: {
+        3: "3px",
+      },
+    },
   },
   plugins: [],
 }
